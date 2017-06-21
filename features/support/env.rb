cdcf5493d2011e0e10bac392d3369931dba7e3db
Benji <benjamincomley@gmail.com>
2017-06-21 13:26:31 +0200

    minor updates

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,7 +12,6 @@ rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
-
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
 World(FactoryGirl::Syntax::Methods)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,16 +5,17 @@ Coveralls.wear_merged!('rails')
 
 RSpec.configure do |config|
 
-  config.before(:each) do
-    Aws.config[:s3] = {stub_responses: true}
-  end
-
   config.expect_with :rspec do |expectations|
+
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.before(:each) do
+    Aws.config[:s3] = {stub_responses: true}
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
